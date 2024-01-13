44c8fafe10e563cebe7fbb05cdd3c2b9be97047f
hostr777 <divakovbogdan11@gmail.com>
2024-01-13 16:17:37 +0300

    Update

---
+++ js/Mollenhauer.js
@@ -0,0 +1,23 @@
+// let cardNumber = "2034399002125581";
+// let cardHidden = "*";
+
+// const getHiddenCard = () => {
+//   console.log(cardNumber.replace("203439900212", cardHidden.repeat(4)));
+// };
+
+// const isInternationalPhone = (number) => {
+//   const firstLetter = number[0];
+//   console.log(firstLetter.toUpperCase() === firstLetter);
+// };
+
+// isInternationalPhone();
+
+// const normalizeUrl = (urlSite = 'https://', address = 'createx.com') => {
+//   if (urlSite.startsWith('https://').address) {
+// 	console.log(urlSite);
+//   } else {
+//     console.log("error");
+//   }
+// };
+
+// normalizeUrl();
