f5b74c253091a1637842cc7b67ca88698cb7473b
hostr777 <divakovbogdan11@gmail.com>
2024-01-19 09:32:33 +0300

    Update

--- js/Mollenhauer.js
+++ js/Mollenhauer.js
@@ -12,12 +12,55 @@
 
 // isInternationalPhone();
 
-// const normalizeUrl = (urlSite = 'https://', address = 'createx.com') => {
-//   if (urlSite.startsWith('https://').address) {
-// 	console.log(urlSite);
-//   } else {
-//     console.log("error");
+// const normalizeUrl = (site) => {
+//     let normalizedUrl;
+  
+//     if (site.startsWith('https://')) {
+//       normalizedUrl = site;
+//     } else {
+//       normalizedUrl = `https://${site}`;
+//     }
+  
+//     return normalizedUrl;
+// }
+
+// normalizeUrl('createx.com');
+
+// const convertText = (text) => {
+//     if (text === '') {
+//       return '';d
+//     }
+  
+//     const reversable = text[0] !== text[0].toUpperCase();
+//     return reversable ? reverse(text) : text;
+//   };
+
+// convertText('');
+
+const getNumberExplanation = () => {
+  // if (666 === 'devil number') {
+  //   console.log('devil number');
+  // } else if (42 ==='answer for everything') {
+  //   console.log('answer for everything');
+  // } else if (7 === 'prime number') {
+  //   console.log('prime number');
+  // } else {
+  //   console.log('error')
+  // }
+
+//   switch (getNumberExplanation) {
+//     case 666:
+//       console.log('devil number');
+//       break;
+//     case 42:
+//       console.log('answer for everything');
+//       break;
+//     case 7:
+//       console.log('prime number');
+//       break;
+//     default:
+//       console.log('error');
 //   }
 // };
 
-// normalizeUrl();
+// getNumberExplanation();
